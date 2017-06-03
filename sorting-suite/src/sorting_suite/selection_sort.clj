3cb6b1d8642f5fc430b113e01e8aa5e1d9d532ff
pwentz <patrick.w.wentz@gmail.com>
2017-06-03 16:14:52 -0500

    immutable, functional selection sort solution.

--- sorting-suite/src/sorting_suite/selection_sort.clj
+++ sorting-suite/src/sorting_suite/selection_sort.clj
@@ -1,18 +1,24 @@
 (ns sorting-suite.selection-sort)
 
-(defn find-lowest
-  ([low [x & xs]]
-   (if x
-     (if (< x low)
-       (find-lowest x xs)
-       (find-lowest low xs))
-     low))
-  ([[x & _ :as c]] (find-lowest x c)))
+(defn find-lowest-idx
+  ([curr low c]
+   (let [x (get c curr)
+         lowest (get c low)]
+     (if x
+       (if (< x lowest)
+         (find-lowest-idx (inc curr) curr c)
+         (find-lowest-idx (inc curr) low c))
+       low)))
+  ([c] (find-lowest-idx 0 0 c)))
 
-(defn selection-sort [[x & xs :as nums]]
-  (when x
-    (let [xs (vec xs)
-          lowest (find-lowest x nums)]
+(defn selection-sort [c]
+  "Original SS implementation mutates values of initial array reference.
+   Since this is a functional solution, immutability is favored.
+   Tradeoff for immutability is stack space taken by recursive calls."
+  (when (seq c)
+    (let [[x & xs :as nums] (vec c)
+          low-idx (find-lowest-idx nums)
+          lowest (get nums low-idx)]
       (if (not= x lowest)
-        (cons lowest (selection-sort (assoc xs (.indexOf xs lowest) x)))
+        (cons lowest (selection-sort (rest (assoc nums low-idx x))))
         (cons x (selection-sort xs))))))
