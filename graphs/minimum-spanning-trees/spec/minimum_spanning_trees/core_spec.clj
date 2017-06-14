2f2e55a116ee1772fe7865e4d1c14fc55bfb74c3
pwentz <patrick.w.wentz@gmail.com>
2017-06-14 17:57:53 -0500

    completes functionality for union find and kruskals algorithm

--- graphs/minimum-spanning-trees/spec/minimum_spanning_trees/core_spec.clj
+++ graphs/minimum-spanning-trees/spec/minimum_spanning_trees/core_spec.clj
@@ -50,3 +50,85 @@
                (:edges (prims graph)))
       (should= 23
                (:total (prims graph))))))
+
+(describe "sort-by-weight"
+  (it "sorts edges of graph by weight"
+    (let [unsorted {#{"E" "C"} 2
+                    #{"E" "F"} 2
+                    #{"D" "E"} 3
+                    #{"C" "D"} 4
+                    #{"D" "G"} 4
+                    #{"A" "B"} 5
+                    #{"C" "F"} 5
+                    #{"A" "D"} 7
+                    #{"B" "C"} 7
+                    #{"E" "G"} 7
+                    #{"B" "D"} 9
+                    #{"A" "G"} 12}
+          expected (into (sorted-map-by #(<= (unsorted %1) (unsorted %2))) unsorted)]
+      (should== expected
+                (sort-by-weight graph)))))
+
+(describe "sorted-dissoc"
+  (it "dissociates"
+    (let [example {#{"E" "C"} 2
+                   #{"M" "N"} 5
+                   #{"D" "G"} 11}
+          sorted (into (sorted-map-by #(<= (example %1) (example %2))) example)]
+      ; (should= {#{"E" "C"} 2
+      ;           #{"D" "G"} 11}
+      ;          (sorted-dissoc sorted #{"M" "N"}))
+      )))
+
+(describe "union-find data structure"
+  (it "can tell if set values for keys in vertices are empty"
+    (let [union-find {"A" #{"D"} "B" #{} "C" #{}}]
+      (should= true
+               (empty-children? union-find #{"B" "C"}))
+      (should= false
+               (empty-children? union-find #{"A" "C"}))))
+
+  (it "can add together empty elements"
+    (let [union-find {"A" #{} "B" #{} "C" #{} "D" #{}}]
+      (should= {"A" #{} "B" #{} "C" #{"A"} "D" #{}}
+               (unionize union-find #{"A" "C"}))))
+
+  (it "can add together non-empty elements"
+    (let [union-find {"A" #{"D"} "B" #{} "C" #{} "D" #{}}]
+      (should= {"A" #{"C" "D"} "B" #{} "C" #{} "D" #{}}
+               (unionize union-find #{"A" "C"}))))
+
+  (it "can unionize elements already belonging to tree"
+    (let [union-find {"A" #{"B" "C"} "B" #{} "C" #{} "D" #{}}]
+      (should= {"A" #{"B" "C" "D"} "B" #{} "C" #{} "D" #{}}
+               (unionize union-find #{"B" "D"}))))
+
+  (it "can tell if elements belong to same set"
+    (let [union-find {"A" #{"B" "C" "H"} "G" #{"N"}}
+          edge-case {"A" #{} "B" #{"A"} "C" #{"B" "D"} "D" #{}}]
+      (should= true
+               (same-set? union-find #{"H" "C"}))
+      (should= true
+               (same-set? union-find #{"A" "B"}))
+      (should= true
+               (same-set? edge-case #{"A" "D"}))
+      )
+    )
+
+  (it "can start elements off"
+    (let [expected {"A" #{} "B" #{} "C" #{} "D" #{} "E" #{} "F" #{} "G" #{}}]
+      (should= expected
+               (add-sets graph)))))
+
+(describe "kruskals"
+  (it "is Kruskal's Algorithm"
+    (let [expected-edges {#{"E" "C"} 2
+                          #{"E" "F"} 2
+                          #{"D" "E"} 3
+                          #{"D" "G"} 4
+                          #{"A" "B"} 5
+                          #{"B" "C"} 7}]
+      (should= expected-edges
+               (:edges (kruskals graph)))
+      (should= 23
+               (:total (kruskals graph))))))
--- graphs/minimum-spanning-trees/src/minimum_spanning_trees/core.clj
+++ graphs/minimum-spanning-trees/src/minimum_spanning_trees/core.clj
@@ -1,4 +1,5 @@
-(ns minimum-spanning-trees.core)
+(ns minimum-spanning-trees.core
+  (:require [clojure.set :as s]))
 
 (defn enqueue [queue edge]
   "Simple priority queue to sort by weight ascending"
@@ -31,3 +32,55 @@
                                         (cons (get-edge-maps graph start (-> start graph keys))))
                               #{start})]
              (assoc {} :edges edges :total (->> edges vals (reduce +))))))
+
+(defn- format-edges [graph]
+  (reduce (fn [acc v]
+            (merge acc
+                   (reduce
+                     #(merge %1 {#{v %2} ((graph v) %2)}) {} (keys (graph v))))) {} (keys graph)))
+
+(defn sort-by-weight [graph]
+  (let [edges (format-edges graph)]
+    (into (sorted-map-by #(<= (edges %1) (edges %2))) edges)))
+
+(defn sorted-dissoc [graph vertices]
+  (let [unsorted (reduce (fn [acc [elt-key elt-val]]
+                           (if (= elt-key vertices)
+                             acc
+                             (assoc acc elt-key elt-val))) {} graph)]
+    (into (sorted-map-by #(<= (unsorted %1) (unsorted %2))) unsorted)))
+
+; TODO:
+  ; - Take all parents/keys that contains either of vertices...
+  ;   ...pick one that contains most children
+  ; - if no parents/keys contains either of vertices, pick one vertex to make parent of other
+(defn unionize [union-find vertices]
+  (let [direct-parents (remove #(empty? (s/intersection vertices (union-find %))) (keys union-find))
+        [main-parent] (reverse (sort-by #(count (union-find %)) direct-parents))]
+    (if main-parent
+      (assoc union-find main-parent (s/union vertices (union-find main-parent)))
+      (let [[largest-parent] (reverse (sort-by #(count (union-find %)) (filter #(contains? vertices %) (keys union-find))))]
+        (assoc union-find largest-parent (s/union (union-find largest-parent) (disj vertices largest-parent)))))))
+
+(defn empty-children? [union-find vertices]
+  (let [v1 (first vertices)
+        v2 (first (disj vertices v1))]
+    (and (empty? (union-find v1)) (empty? (union-find v2)))))
+
+(defn same-set? [union-find vertices]
+  (let [parent-nodes (remove #(empty? (s/intersection vertices (union-find %))) (keys union-find))
+        all-siblings (reduce #(s/union %1 (union-find %2) #{%2}) #{} parent-nodes)]
+    (s/subset? vertices all-siblings)))
+
+(defn add-sets [graph]
+  (reduce #(merge %1 {%2 #{}}) {} (keys graph)))
+
+(defn kruskals
+  ([traversed union-find edges]
+   (let [[vertices weight] (first edges)]
+     (cond (nil? vertices) traversed
+           (same-set? union-find vertices) (recur traversed union-find (sorted-dissoc edges vertices))
+           :else (recur (merge traversed {vertices weight}) (unionize union-find vertices) (sorted-dissoc edges vertices))
+           )))
+  ([graph] (let [edges (kruskals {} (add-sets graph) (sort-by-weight graph))]
+             {:edges edges :total (reduce + (vals edges))})))
