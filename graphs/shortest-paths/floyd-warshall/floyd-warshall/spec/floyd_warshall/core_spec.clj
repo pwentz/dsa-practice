039c714cd764d4d13524a018370cba84ea30316e
pwentz <patrick.w.wentz@gmail.com>
2017-06-20 19:29:59 -0500

    functional implementation of floyd-warshalls

--- graphs/shortest-paths/floyd-warshall/floyd-warshall/spec/floyd_warshall/core_spec.clj
+++ graphs/shortest-paths/floyd-warshall/floyd-warshall/spec/floyd_warshall/core_spec.clj
@@ -6,18 +6,31 @@
 ; TODO:
   ; - Consider adding support for undirected graphs
   ; (merge-with merge graph {source {neighbor weight}} {neighbor {source weight}})
+
+; TODO: FIND WHAT YOU WANT FINAL FN TO RETURN (parents-matrix is useless)
 (defn add-edge [graph source neighbor weight]
   (merge-with merge graph {source {neighbor weight}}))
 
 (def i Long/MAX_VALUE)
 
 (def mini-graph
-  (-> (add-edge {} 3 4 -5)
-      (add-edge 2 4 -2)
-      (add-edge 1 4 3)
-      (add-edge 1 2 4)
+  (-> (add-edge {} 1 2 4)
       (add-edge 1 3 1)
-      (add-edge 2 3 8)))
+      (add-edge 1 4 3)
+      (add-edge 2 3 8)
+      (add-edge 2 4 -2)
+      (add-edge 3 4 -5)))
+
+(def graph
+  (-> (add-edge {} 1 2 3)
+      (add-edge 1 5 -4)
+      (add-edge 1 3 8)
+      (add-edge 2 4 1)
+      (add-edge 2 5 7)
+      (add-edge 3 2 4)
+      (add-edge 4 1 2)
+      (add-edge 4 3 -5)
+      (add-edge 5 4 6)))
 
 (describe "adjacency-matrix"
   (it "creates an adjacency matrix from a graph"
@@ -38,3 +51,21 @@
                   [i  i  i  x]]]
       (should= result
                (parent-matrix distance-matrix)))))
+
+(describe "floyd-warshall"
+  (it "can handle a smaller graph"
+    (let [result [[0  4  1  -4]
+                  [i  0  8  -2]
+                  [i  i  0  -5]
+                  [i  i  i   0]]]
+      (should= result
+               (floyd-warshall mini-graph))))
+
+  (it "can do larger graphs"
+    (let [result [[0   1  -3   2  -4]
+                  [3   0  -4   1  -1]
+                  [7   4   0   5   3]
+                  [2  -1  -5   0  -2]
+                  [8   5   1   6   0]]]
+      (should= result
+               (floyd-warshall graph)))))
--- graphs/shortest-paths/floyd-warshall/floyd-warshall/src/floyd_warshall/core.clj
+++ graphs/shortest-paths/floyd-warshall/floyd-warshall/src/floyd_warshall/core.clj
@@ -2,25 +2,40 @@
 
 (def inf Long/MAX_VALUE)
 
-(defn- vertices [graph]
-  (let [all-vertices (apply conj (-> graph keys set) (mapcat (comp keys graph) (keys graph)))]
-    (sort-by (comp - count graph) all-vertices)))
+(defn- get-vertices [graph]
+  (distinct (concat (keys graph) (mapcat (comp keys graph) (keys graph)))))
 
 (defn create-row [graph vertex]
   (let [neighbors (graph vertex)
-        positions (zipmap (range) (vertices graph))]
-    (map (fn [idx]
-           (if (= vertex (positions idx))
-             0
-             (get neighbors (positions idx) inf)))
-         (-> positions count range))))
+        positions (zipmap (range) (get-vertices graph))]
+    (mapv (fn [idx]
+            (if (= vertex (positions idx))
+              0
+              (get neighbors (positions idx) inf)))
+          (-> positions count range))))
 
 (defn adjacency-matrix [graph]
-  (reduce #(conj %1 (create-row graph %2)) [] (vertices graph)))
+  (reduce #(conj %1 (create-row graph %2)) [] (get-vertices graph)))
 
 (defn parent-matrix [distances]
   (letfn [(parent-row [row-idx row]
-            (map #(cond (zero? %) nil
-                        (< % inf) row-idx
-                        :else %) row))]
-    (map-indexed parent-row distances)))
+            (mapv #(cond (zero? %) nil
+                         (< % inf) row-idx
+                         :else %) row))]
+    (vec (map-indexed parent-row distances))))
+
+(defn floyd-warshall
+  ([distances intermediate-idx start-idx end-idx]
+   (let [current-best (get-in distances [start-idx end-idx])
+         start-to-intermediate (get-in distances [start-idx intermediate-idx])
+         intermediate-to-end (get-in distances [intermediate-idx end-idx])
+         new-cost (if (or (= inf start-to-intermediate) (= inf intermediate-to-end)) inf (+ start-to-intermediate intermediate-to-end))]
+     (if (< new-cost current-best)
+       (assoc-in distances [start-idx end-idx] new-cost)
+       distances)))
+  ([distances intermediate-idx start-idx]
+   (reduce #(floyd-warshall %1 intermediate-idx start-idx %2) distances (-> distances count range)))
+  ([distances intermediate-idx]
+   (reduce #(floyd-warshall %1 intermediate-idx %2) distances (-> distances count range)))
+  ([graph] (let [distances (adjacency-matrix graph)]
+             (reduce floyd-warshall distances (-> distances count range)))))
